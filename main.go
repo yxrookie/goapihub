ef8d11b6c55765a2f739264f381a00e4c1513f73
rookieyx <lyxrookie@qq.com>
2024-03-04 14:55:09 +0800

    the configuration of viper and cast

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"goapihub/bootstrap"
+	"goapihub/config"
+	pkgconfig "goapihub/pkg/config"
+
 	"github.com/gin-gonic/gin"
 )
 
+func init () {
+	config.Initialize()
+}
+
 func main() {
 
-    // new 一个 Gin Engine 实例
-    router := gin.New()
-
-    // 初始化路由绑定
-    bootstrap.SetupRoute(router)
-
-    // 运行服务
-    err := router.Run(":3000")
-    if err != nil {
-        // 错误处理，端口被占用了或者其他错误
-        fmt.Println(err.Error())
-    }
+       // 配置初始化，依赖命令行 --env 参数
+	   var env string
+	   flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	   flag.Parse()
+	   pkgconfig.InitConfig(env)
+   
+	   // new 一个 Gin Engine 实例
+	   router := gin.New()
+   
+	   // 初始化路由绑定
+	   bootstrap.SetupRoute(router)
+   
+	   // 运行服务
+	   err := router.Run(":" + pkgconfig.Get("app.port"))
+	   if err != nil {
+		   // 错误处理，端口被占用了或者其他错误
+		   fmt.Println(err.Error())
+	   }
 }
