d940e7a45ba7ca0ef4bb54ea8a9e326c7a180087
rookieyx <lyxrookie@qq.com>
2024-03-11 17:55:05 +0800

    fix bug: new user's date is null when register

--- app/models/model.go
+++ app/models/model.go
@@ -4,6 +4,7 @@ package models
 import (
 	"time"
 
+	"gorm.io/gorm"
 )
 
 // BaseModel: model base class
@@ -16,3 +17,16 @@ type CommonTimestampField struct {
 	CreateAt time.Time `gorm:"column:created_at;index;" json:"created_at,omitempty"`
 	UpdatedAt time.Time `gorm:"column:updated_at;index;" json:"updated_at,omitempty"`
 }
+
+// 在模型中定义 BeforeCreate 方法，确保在创建记录前更新 CreatedAt 字段
+func (ctf *CommonTimestampField) BeforeCreate(tx *gorm.DB) (err error) {
+    ctf.CreateAt = time.Now()
+    return nil
+}
+
+// 在模型中定义 BeforeSave 方法，确保在保存记录前更新 UpdatedAt 字段
+func (ctf *CommonTimestampField) BeforeSave(tx *gorm.DB) (err error) {
+    ctf.UpdatedAt = time.Now()
+    return nil
+}
+
