d106b2020a60e796e8a13fb1bc05d8062ef22952
rookieyx <lyxrookie@qq.com>
2024-03-13 16:59:01 +0800

    auth middleware

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	"goapihub/app/http/middlewares"
 	"goapihub/bootstrap"
 	"goapihub/config"
+	"goapihub/pkg/auth"
 	pkgconfig "goapihub/pkg/config"
-	
+	"goapihub/pkg/response"
+
 	"github.com/gin-gonic/gin"
 )
 
