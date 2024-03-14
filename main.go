95bc9d88c95c3c6628b05593903849ed1ac6e8c4
rookieyx <lyxrookie@qq.com>
2024-03-14 15:27:47 +0800

    make cmd command

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "goapihub/app/cmd"
+	"goapihub/app/cmd/make"
     "goapihub/bootstrap"
     btsConig "goapihub/config"
     "goapihub/pkg/config"
@@ -49,6 +50,7 @@ func main() {
         cmd.CmdServe,
 		cmd.CmdKey,
 		cmd.CmdPlay,
+		make.CmdMake,
     )
 
     // 配置默认运行 Web 服务
